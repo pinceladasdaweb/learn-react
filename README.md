c3d6477ab9b6ea8ee62baa295aec69dc8fd6bf95
Pedro Rogério <pinceladasdaweb@hotmail.com>
2015-05-30 21:49:38 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learn React
+
+Basic examples to learn React
---
+++ hello-world/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var Hello = React.createClass({
+    render: function() {
+        return <h1>Hello World by React!</h1>
+    }
+});
